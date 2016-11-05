d9ee006f89ec9f7bb8e3227454e4ee146d9d09c6
Tlareg Aivir <tlaregaivir@gmail.com>
2016-11-05 20:46:46 +0100

    chart

--- src/app.js
+++ src/app.js
@@ -1,4 +1,30 @@
 import './styles/main.css';
+import 'c3/c3.css';
+
+import c3 from 'c3';
 import weightData from './data/weight-data.json';
 
-console.log(weightData.timeline.length);
+const { timeline } = weightData; 
+
+const chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    x: 'date',
+    columns: [
+      ['date', ...timeline.map(i => i.date)],
+      ['weight', ...timeline.map(i => parseFloat(i.weight))]
+    ],
+    types: {
+      'weight': 'spline'
+    }
+  },
+  axis: {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%Y-%m-%d',
+        rotate: 45
+      }
+    }
+  }
+});
