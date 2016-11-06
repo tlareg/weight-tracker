80ee3f4f5a1b7ac10e26b5a4b191cad29d168d7b
Tlareg Aivir <tlaregaivir@gmail.com>
2016-11-06 21:36:38 +0100

    diff and speed charts

--- src/app.js
+++ src/app.js
@@ -4,26 +4,133 @@ import 'c3/c3.css';
 import c3 from 'c3';
 import weightData from './data/weight-data.json';
 
-const { timeline } = weightData; 
+const { timeline } = weightData;
 
-const chart = c3.generate({
-  bindto: '#chart',
+let dates = [];
+let weights = [];
+let weightDiffs = [];
+let weightDiffSpeeds = [];
+
+(function processWeightTimeline() {
+  timeline.reduce(({ prevWeight, prevDateStr }, item) => {
+
+    const dateStr = item.date;
+    dates.push(dateStr);
+
+    const weight = parseFloat(item.weight);
+    weights.push(weight);
+
+    const weightDiff = prevWeight 
+      ? round100(weight - prevWeight, -2)
+      : 0;
+    weightDiffs.push(weightDiff);
+
+    const dateDaysDiff = prevDateStr 
+      ? dayDiff(new Date(prevDateStr), new Date(dateStr))
+      : 0;
+    const weightDiffSpeed = dateDaysDiff
+      ? round100(weightDiff / dateDaysDiff)
+      : 0;
+
+    weightDiffSpeeds.push(weightDiffSpeed);
+    
+    return {
+      prevWeight: weight,
+      prevDateStr: dateStr
+    }
+  }, {});
+})();
+
+function round100(number) {
+  return Math.round(number * 100) / 100;
+}
+
+function dayDiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+const dateAxisConfig = {
+  label: {
+    text: 'date',
+    position: 'inner-center'
+  },
+  type: 'timeseries',
+  tick: {
+    format: '%Y-%m-%d',
+    rotate: 45
+  }
+};
+
+const weightChart = c3.generate({
+  bindto: '.weight-chart',
   data: {
     x: 'date',
+    y: 'weight',
     columns: [
-      ['date', ...timeline.map(i => i.date)],
-      ['weight', ...timeline.map(i => parseFloat(i.weight))]
+      ['date', ...dates],
+      ['weight', ...weights]
     ],
     types: {
       'weight': 'spline'
     }
   },
   axis: {
-    x: {
-      type: 'timeseries',
+    x: dateAxisConfig,
+    y: {
+      label: {
+        text: 'weight [kg]',
+        position: 'outer-middle'
+      }
+    }
+  }
+});
+
+const weightDiffChart = c3.generate({
+  bindto: '.weight-diff-chart',
+  data: {
+    x: 'date',
+    y: 'weight_diff',
+    columns: [
+      ['date', ...dates],
+      ['weight_diff', ...weightDiffs]
+    ],
+    types: {
+      'weight_diff': 'spline'
+    }
+  },
+  axis: {
+    x: dateAxisConfig,
+    y: {
+      label: {
+        text: 'weight diff [kg]',
+        position: 'outer-middle'
+      }
+    }
+  }
+});
+
+const weightDiffSpeedChart = c3.generate({
+  bindto: '.weight-diff-speed-chart',
+  data: {
+    x: 'date',
+    y: 'weight_diff_speed',
+    columns: [
+      ['date', ...dates],
+      ['weight_diff_speed', ...weightDiffSpeeds]
+    ],
+    types: {
+      'weight_diff_speed': 'area-step'
+    }
+  },
+  axis: {
+    x: dateAxisConfig,
+    y: {
+      label: {
+        text: 'weight diff speed [kg/day]',
+        position: 'outer-middle'
+      },
       tick: {
-        format: '%Y-%m-%d',
-        rotate: 45
+        format: d => round100(d)
       }
     }
   }
