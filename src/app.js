4af53ba648cef2303a3539f27d18d805e4f26321
tlareg <tlaregaivir@gmail.com>
2021-03-25 09:40:56 +0100

    cache timestamp, prettier, add dates from to

--- src/app.js
+++ src/app.js
@@ -1,88 +1,114 @@
-import './styles/main.css';
-import 'c3/c3.css';
+import "./styles/main.css";
+import "c3/c3.css";
 
-import c3 from 'c3';
-import weightData from './data/weight-data.json';
+import c3 from "c3";
+import weightData from "./data/weight-data.json";
 
-const averageStepInput = document.querySelector('.average-step-input')
-const refreshBtn = document.querySelector('.refresh-btn')
+const averageStepInput = document.querySelector(".average-step-input");
+const dateFromInput = document.querySelector(".date-from-input");
+const dateToInput = document.querySelector(".date-to-input");
+const refreshBtn = document.querySelector(".refresh-btn");
 
-refreshBtn.addEventListener('click', () => {
-  const averageStep = averageStepInput.value && parseInt(averageStepInput.value, 10) || 1
+refreshBtn.addEventListener("click", () => {
+  const averageStep =
+    (averageStepInput.value && parseInt(averageStepInput.value, 10)) || 1;
+  const dateFrom = dateFromInput.value;
+  const dateTo = dateToInput.value;
+
   refresh({
     weightData,
-    averageStep
-  })
-})
+    averageStep,
+    dateFrom,
+    dateTo,
+  });
+});
 
-
-// TODO: choosing dates from to
 refresh({
   weightData,
-  averageStep: 1
-})
+  averageStep: 1,
+});
 
-function refresh(inputs) {
-  const { weightData, averageStep } = inputs
+function refresh({ weightData, averageStep, dateFrom, dateTo }) {
   const { timeline } = weightData;
-  const averageTimeline = createAverageTimeline(timeline, averageStep)
-  
-  const { 
-    dates, 
-    weights, 
-    weightDiffs, 
-    weightDiffSpeeds
+
+  const from = dateFrom && new Date(dateFrom).getTime();
+  const to = dateTo && new Date(dateTo).getTime();
+
+  const parsedTimeline = timeline.map((item) =>
+    Object.assign({}, item, { timestamp: new Date(item.date).getTime() })
+  );
+
+  const timelineInDateRange = parsedTimeline.filter(
+    ({ timestamp: current }) =>
+      (from ? from <= current : true) && (to ? current <= to : true)
+  );
+
+  const averageTimeline = createAverageTimeline(
+    timelineInDateRange,
+    averageStep
+  );
+
+  const {
+    dates,
+    weights,
+    weightDiffs,
+    weightDiffSpeeds,
   } = processWeightTimeline(averageTimeline);
 
-  generateCharts({ 
-    dates, 
-    weights, 
-    weightDiffs, 
-    weightDiffSpeeds
+  generateCharts({
+    dates,
+    weights,
+    weightDiffs,
+    weightDiffSpeeds,
   });
 }
 
 function createAverageTimeline(originTimeline, averageStep) {
-  return originTimeline.reduce(({ newTimeline, averageItems }, item, index) => {
-    
-    const firstItemOfAverage = averageItems[0]
-    if (!firstItemOfAverage) {
-      return { newTimeline, averageItems: [item] }
-    } 
-    
-    const isLastItem = index === originTimeline.length - 1
+  return originTimeline.reduce(
+    ({ newTimeline, averageItems }, item, index) => {
+      const firstItemOfAverage = averageItems[0];
+      if (!firstItemOfAverage) {
+        return { newTimeline, averageItems: [item] };
+      }
 
-    const dateDaysDiff = dayDiff(
-      new Date(firstItemOfAverage.date),
-      new Date(item.date)
-    )
+      const isLastItem = index === originTimeline.length - 1;
 
-    if (dateDaysDiff < averageStep) {
-      averageItems = [...averageItems, item]
-      if (!isLastItem) {
-        return { newTimeline, averageItems }
-      } 
-      return { newTimeline: [...newTimeline, createAverageWeightItem(averageItems)] } 
-    }
+      const dateDaysDiff = dayDiff(
+        firstItemOfAverage.timestamp,
+        item.timestamp
+      );
 
-    newTimeline = [...newTimeline, createAverageWeightItem(averageItems)]
-    if (isLastItem) {
-      newTimeline = [...newTimeline, item]
-    }
-    return {
-      newTimeline,
-      averageItems: [item]
-    }
-  }, { newTimeline: [], averageItems: [] }).newTimeline
+      if (dateDaysDiff < averageStep) {
+        averageItems.push(item);
+        if (!isLastItem) {
+          return { newTimeline, averageItems };
+        }
+        newTimeline.push(createAverageWeightItem(averageItems));
+        return { newTimeline };
+      }
+
+      newTimeline.push(createAverageWeightItem(averageItems));
+      if (isLastItem) {
+        newTimeline.push(item);
+      }
+      return {
+        newTimeline,
+        averageItems: [item],
+      };
+    },
+    { newTimeline: [], averageItems: [] }
+  ).newTimeline;
 }
 
 function createAverageWeightItem(items) {
-  const sum = items.reduce((acc, item) => acc + parseFloat(item.weight), 0)
-  const averageWeight = round100(sum / items.length)
+  const sum = items.reduce((acc, item) => acc + parseFloat(item.weight), 0);
+  const averageWeight = round100(sum / items.length);
+  const lastItem = items[items.length -1];
   return {
-    date: items[items.length - 1].date,
-    weight: '' + averageWeight
-  }
+    date: lastItem.date,
+    timestamp: lastItem.timestamp,
+    weight: "" + averageWeight,
+  };
 }
 
 function processWeightTimeline(timeline) {
@@ -91,43 +117,33 @@ function processWeightTimeline(timeline) {
   let weightDiffs = [];
   let weightDiffSpeeds = [];
 
-  let totalDays = 0;
-  let totalWeightDiff = 0;
-  let averageWeightDiffSpeed = 0; 
-
-  timeline.reduce(({ prevWeight, prevDateStr }, item) => {
-
-    const dateStr = item.date;
+  timeline.reduce(({ prevWeight, prevTimestamp }, item) => {
+    const { date: dateStr, timestamp } = item;
     dates.push(dateStr);
 
     const weight = parseFloat(item.weight);
     weights.push(weight);
 
-    const weightDiff = prevWeight 
-      ? round100(weight - prevWeight, -2)
-      : 0;
+    const weightDiff = prevWeight ? round100(weight - prevWeight, -2) : 0;
     weightDiffs.push(weightDiff);
 
-    const dateDaysDiff = prevDateStr 
-      ? dayDiff(new Date(prevDateStr), new Date(dateStr))
-      : 0;
+    const dateDaysDiff = prevTimestamp ? dayDiff(prevTimestamp, timestamp) : 0;
     const weightDiffSpeed = dateDaysDiff
       ? round100(weightDiff / dateDaysDiff)
       : 0;
-
     weightDiffSpeeds.push(weightDiffSpeed);
-    
+
     return {
       prevWeight: weight,
-      prevDateStr: dateStr
-    }
+      prevTimestamp: timestamp,
+    };
   }, {});
 
-  return { 
-    dates, 
-    weights, 
-    weightDiffs, 
-    weightDiffSpeeds
+  return {
+    dates,
+    weights,
+    weightDiffs,
+    weightDiffSpeeds,
   };
 }
 
@@ -135,112 +151,115 @@ function round100(number) {
   return Math.round(number * 100) / 100;
 }
 
-function dayDiff(first, second) {
-    return Math.round( (second - first) / (1000 * 60 * 60 * 24) );
+function dayDiff(firstTime, secondTime) {
+  return Math.round((secondTime - firstTime) / (1000 * 60 * 60 * 24));
 }
 
-function generateCharts({ dates, weights, weightDiffs, weightDiffSpeeds}) {
+function generateCharts({ dates, weights, weightDiffs, weightDiffSpeeds }) {
   const dateAxisConfig = {
     label: {
-      text: 'date',
-      position: 'inner-center'
+      text: "date",
+      position: "inner-center",
     },
-    type: 'timeseries',
+    type: "timeseries",
     tick: {
-      format: '%Y-%m-%d',
-      rotate: 45
-    }
+      format: "%Y-%m-%d",
+      rotate: 45,
+    },
   };
 
   generateWeightChart({ dates, weights, dateAxisConfig });
-  generateWeightDiffChart({ dates, weightDiffs, dateAxisConfig});
-  generateWeightDiffSpeedChart({ dates, weightDiffSpeeds, dateAxisConfig});
+  generateWeightDiffChart({ dates, weightDiffs, dateAxisConfig });
+  generateWeightDiffSpeedChart({ dates, weightDiffSpeeds, dateAxisConfig });
 }
-
 
 function generateWeightChart({ dates, weights, dateAxisConfig }) {
   return c3.generate({
-    bindto: '.weight-chart',
+    bindto: ".weight-chart",
     size: {
       height: 960,
     },
     data: {
-      x: 'date',
-      y: 'weight',
+      x: "date",
+      y: "weight",
       columns: [
-        ['date', ...dates],
-        ['weight', ...weights]
+        ["date", ...dates],
+        ["weight", ...weights],
       ],
       types: {
-        'weight': 'spline'
-      }
+        weight: "line",
+      },
     },
     axis: {
       x: dateAxisConfig,
       y: {
         label: {
-          text: 'weight [kg]',
-          position: 'outer-middle'
-        }
-      }
-    }
+          text: "weight [kg]",
+          position: "outer-middle",
+        },
+      },
+    },
   });
 }
 
-function generateWeightDiffChart({ dates, weightDiffs, dateAxisConfig}) {
+function generateWeightDiffChart({ dates, weightDiffs, dateAxisConfig }) {
   return c3.generate({
-    bindto: '.weight-diff-chart',
+    bindto: ".weight-diff-chart",
     size: {
       height: 250,
     },
     data: {
-      x: 'date',
-      y: 'weight_diff',
+      x: "date",
+      y: "weight_diff",
       columns: [
-        ['date', ...dates],
-        ['weight_diff', ...weightDiffs]
+        ["date", ...dates],
+        ["weight_diff", ...weightDiffs],
       ],
       types: {
-        'weight_diff': 'spline'
-      }
+        weight_diff: "line",
+      },
     },
     axis: {
       x: dateAxisConfig,
       y: {
         label: {
-          text: 'weight diff [kg]',
-          position: 'outer-middle'
-        }
-      }
-    }
+          text: "weight diff [kg]",
+          position: "outer-middle",
+        },
+      },
+    },
   });
 }
 
-function generateWeightDiffSpeedChart({ dates, weightDiffSpeeds, dateAxisConfig}) {
+function generateWeightDiffSpeedChart({
+  dates,
+  weightDiffSpeeds,
+  dateAxisConfig,
+}) {
   return c3.generate({
-    bindto: '.weight-diff-speed-chart',
+    bindto: ".weight-diff-speed-chart",
     data: {
-      x: 'date',
-      y: 'weight_diff_speed',
+      x: "date",
+      y: "weight_diff_speed",
       columns: [
-        ['date', ...dates],
-        ['weight_diff_speed', ...weightDiffSpeeds]
+        ["date", ...dates],
+        ["weight_diff_speed", ...weightDiffSpeeds],
       ],
       types: {
-        'weight_diff_speed': 'area-step'
-      }
+        weight_diff_speed: "area-step",
+      },
     },
     axis: {
       x: dateAxisConfig,
       y: {
         label: {
-          text: 'weight diff speed [kg/day]',
-          position: 'outer-middle'
+          text: "weight diff speed [kg/day]",
+          position: "outer-middle",
         },
         tick: {
-          format: d => round100(d)
-        }
-      }
-    }
+          format: (d) => round100(d),
+        },
+      },
+    },
   });
 }
